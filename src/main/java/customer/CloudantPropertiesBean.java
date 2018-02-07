10b0eeef5c4a0621bca91ef6de7daea6a065b73e
Timo Timo Paananen <timo.paananen@kela.fi>
2018-02-07 14:27:58 +0000

    / day 3 afternoon/

---
+++ src/main/java/customer/CloudantPropertiesBean.java
@@ -0,0 +1,55 @@
+package customer; 
+import org.springframework.boot.context.properties.ConfigurationProperties;  
+import org.springframework.stereotype.Component; 
+
+ @Component 
+ @ConfigurationProperties(prefix="spring.cloudant")
+ public class CloudantPropertiesBean {
+ 	
+ 	private String username;
+ 	private String password;
+ 	private String host;
+ 	private String protocol;
+ 	private int port;
+ 	private String database;
+ 	
+ 	public void setUsername(String username) {
+ 		this.username = username;
+ 	}
+ 	
+ 	public void setPassword(String password) {
+ 		this.password = password;
+ 	}
+ 	
+ 	public void setHost(String h) {
+ 		this.host = h;
+ 	}
+ 	
+ 	public void setProtocol(String p) {
+ 		this.protocol = p;
+ 	}
+ 	
+ 	public void setPort(int p) {
+ 		this.port = p;
+ 		
+ 	}
+ 	
+ 	public void setDatabase(String db){
+ 		this.database = db;
+ 	}
+ 	public String getUsername() {
+ 		return this.username;
+ 	}
+ 	public String getPassword(){
+ 		return this.password;
+ 	}
+ 	public String getHost() {
+ 		return this.host;
+ 	}
+ 	public String getProtocol(){
+ 		return this.protocol;
+ 	}
+ 	public int getPort(){
+ 		return this.port;
+ 		}
+ }
---
+++ src/main/java/customer/Customer.java
@@ -0,0 +1,63 @@
+package customer;
+
+public class Customer {
+	private String _id;
+	private String _rev;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String imageUrl;
+	
+	public String getCustomerId(){
+		return this._id;
+	} 
+	public String getRev(){
+		return this._rev;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+	public String getPassword(){
+		return this.password;
+	}
+	
+	public String getFirstName(){
+		return this.firstName;
+	}
+	public String getLastName(){
+		return this.lastName;
+	}
+	public String getEmail(){
+		return this.email;
+	}
+	public String getImageUrl(){
+		return this.imageUrl;
+	} public void setCustomerId(String id){
+		this._id = id;
+	}
+	
+	public void setRev(String r){
+		this._rev= ;
+	}
+	public void setUsername(String u){
+		this.username = u:
+	}
+	public void setPassword(String p){
+		this.password = p;
+	}
+	public void setFirstName(String n){
+		this.firstName = n;
+	}
+	public void setLastName(String l){
+		this.lastName = l;
+	}
+	public void setEmail(String e){
+		this.email=e;
+	}
+	public void setImageUrl(String i){
+		this.imageUrl = i;
+	}
+}
